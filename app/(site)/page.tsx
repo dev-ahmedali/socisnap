ee92f08c916d4db458395c2b24a3fb22bf61bfb4
dev-ahmedali <ahmedali69325@gmail.com>
2023-05-22 01:52:25 +0600

    auth setup

---
+++ app/(site)/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-full justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Image
+          alt="Socisnap-logo"
+          src="/images/logo.png"
+          width="48"
+          height="48"
+          className="mx-auto w-auto"
+        />
+        <h2 className="mt-6 font-bold text-center text-3xl tracking-tighter text-gray-900">Sign in to your Account</h2>
+      </div>
+      {/* Auth form */}
+    </div>
+  );
+}

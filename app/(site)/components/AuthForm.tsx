db7ee021d13607be293aefaaab376653ffa5dfc7
dev-ahmedali <ahmedali69325@gmail.com>
2023-05-24 15:00:25 +0600

    updated auth ui

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -4,6 +4,8 @@ import Button from "@/app/components/Button";
 import Input from "@/app/components/inputs/Input";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+import { BsGithub, BsGoogle } from "react-icons/bs";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -71,9 +73,45 @@ const AuthForm = () => {
             errors={errors}
           />
           <div>
-            <Button>Test</Button>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === "LOGIN" ? "Sign In" : "Register"}
+            </Button>
           </div>
         </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div className="inset-0 absolute flex items-center">
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="flex relative justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton
+              onClick={() => socialAction("github")}
+              icon={BsGithub}
+            />
+            <AuthSocialButton
+              onClick={() => socialAction("google")}
+              icon={BsGoogle}
+            />
+          </div>
+        </div>
+
+        <div
+          className="flex gap-2 justify-center text-sm px-2 mt-6 text-gray-500
+        ">
+          <div>
+            {variant === "LOGIN"
+              ? "New to Socisnap?"
+              : "Already have an account?"}
+          </div>
+          <div onClick={toggleVariant} className="underline cursor-pointer
+          ">{variant === 'LOGIN' ? "Create an account" : "Already login!"}</div>
+        </div>
       </div>
     </div>
   );
--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -26,11 +26,10 @@ const Button: React.FC<ButtonProps> = ({
       type={type}
       disabled={disabled}
       className={clsx(
-        `
-  flex justify-center rounded-md px-3 py-2 
-  text-sm font-semibold focus-visible:outline
-  focus-visible:outline-2
-  focus-visible:outline-offset-2`,
+        `flex justify-center rounded-md px-3 py-2 
+         text-sm font-semibold focus-visible:outline
+         focus-visible:outline-2
+         focus-visible:outline-offset-2`,
         disabled && "opacity-50 cursor-default",
         fullWidth && "w-full",
         secondary ? "text-gray-900" : "text-white",
@@ -39,7 +38,9 @@ const Button: React.FC<ButtonProps> = ({
         !secondary &&
           !danger &&
           "bg-sky-500 hover:bg-sky-600 focus-visible:outline-sky-600",
-      )}></button>
+      )}>
+      {children}
+    </button>
   );
 };
 

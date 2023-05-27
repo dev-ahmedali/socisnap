7c06dc4e695a5d9ba29bea8b3bb6ab1d088117c3
dev-ahmedali <ahmedali69325@gmail.com>
2023-05-27 13:46:58 +0600

    updated social auth

--- app/(site)/components/AuthSocialButton.tsx
+++ app/(site)/components/AuthSocialButton.tsx
@@ -9,10 +9,11 @@ interface AuthSocialButtonProps {
 
 const AuthSocialButton: React.FC<AuthSocialButtonProps> = ({
   icon: Icon,
-  onClick,
+  onClick
 }) => {
   return (
     <button
+    onClick={onClick}
       className="inline-flex justify-center w-full rounded-md bg-white
     px-4 py-2 text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300
     hover:bg-gray-50 focus:outline-offset-0">

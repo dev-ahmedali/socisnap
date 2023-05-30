43158c189cee95c98d1a23afcd0fa9b6ff57a94a
dev-ahmedali <ahmedali69325@gmail.com>
2023-05-30 23:11:29 +0600

    updated desktop sidebar

---
+++ app/components/sidebar/DesktopItem.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+const DesktopItem = () => {
+    return (
+        <div>Desktop Item</div>
+    )
+}
+
+export default DesktopItem
--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,9 +1,37 @@
+"use client";
 
+import useRoutes from "@/app/hooks/useRoutes";
+import { useState } from "react";
 
 const DesktopSidebar = () => {
-    return(
-        <div>Desktop sidebar</div>
-    )
-}
+  const routes = useRoutes();
+  const [isOpen, setIsOpen] = useState();
+  return (
+    <div
+      className="hidden lg:fixed
+        lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20
+        xl:px-6 lg:overflow-y-auto lg:bg-white
+        lg:border-r-[1px] lg:pb-4 lg:flex
+        lg:flex-col lg:justify-between">
+      <nav
+        className="mt-4 flex flex-col
+      justify-between">
+        <ul
+          role="list"
+          className="flex flex-col items-center
+        space-y-1">
+          {routes.map((item) => (
+            <DesktopItem key={item.label} 
+            href={item.href}
+            label={item.label}
+            icon={item.icon}
+            active={item.active}
+            onclick={item.onClick}
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
-export default DesktopSidebar
+export default DesktopSidebar;

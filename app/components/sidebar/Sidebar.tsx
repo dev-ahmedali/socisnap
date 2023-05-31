022a959cae5841f767c118facfe98e7826f03b1e
dev-ahmedali <ahmedali69325@gmail.com>
2023-06-01 01:21:14 +0600

    updated sidebar component3

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -4,6 +4,7 @@ async function Sidebar({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-full">
         <DesktopSidebar/>
+        <MobileFooter/>
       <main className="lg:pl-20 h-full">{children}</main>
     </div>
   );

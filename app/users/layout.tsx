6cae050cf33d47e0f24d42fc9cd3b60de8abfd33
dev-ahmedali <ahmedali69325@gmail.com>
2023-06-03 02:11:41 +0600

    try fix error

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,13 +1,17 @@
+import getUsers from "../actions/getUsers";
 import Sidebar from "../components/sidebar/Sidebar";
+import UserList from "./components/UserList";
 
 export default async function UsersLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const users = await getUsers;
   return (
     //@ts-expect-error Server Components
     <Sidebar>
+      <UserList item={users}/>
       <div className="h-full">{children}</div>
     </Sidebar>
   );
